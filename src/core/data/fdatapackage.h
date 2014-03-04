aa387f72dd6beff813a43ebb1769366388a199f0
Rafael Fassi Lobao <rafael@fassi.com.br>
2014-03-04 16:30:01 -0300

    Added license in files

--- src/core/data/fdatapackage.h
+++ src/core/data/fdatapackage.h
@@ -1,3 +1,21 @@
+/****************************************************************************
+**
+**Copyright (C) 2010-2014 - Rafael Fassi Lobão
+**Contact: www.fassi.com.br - rafael@fassi.com.br
+**
+**This file is part of the Fassichip.
+**Fassichip is free software; you can redistribute it and/or modify
+**it under the terms of the GNU General Public License as published by
+**the Free Software Foundation; either version 2 of the License, or
+**(at your option) any later version.
+**
+**This program is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**GNU General Public License for more details.
+**
+****************************************************************************/
+
 #ifndef FDATAPACKAGE_H
 #define FDATAPACKAGE_H
 
